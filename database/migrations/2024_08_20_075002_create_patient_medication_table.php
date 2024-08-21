697fd0e58d52a328c7e9f4ad6e4f4aa54ff3c20e
php20Type <php20.department@gmail.com>
2024-08-21 11:17:05 +0530

    added nullble feild in patient medication migration

--- database/migrations/2024_08_20_075002_create_patient_medication_table.php
+++ database/migrations/2024_08_20_075002_create_patient_medication_table.php
@@ -12,14 +12,14 @@ return new class extends Migration {
     {
         Schema::create('patient_medication', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('patient_id'); // Foreign key to patients table
-            $table->string('medication');
-            $table->string('purpose_of_medication');
-            $table->text('use_schedule'); // Can be JSON or TEXT based on your requirement
-            $table->string('food_use');
-            $table->string('dose_use');
-            $table->string('doses_per_package');
-            $table->date('last_prescription_start');
+            $table->unsignedBigInteger('patient_id')->nullable(); // Foreign key to patients table
+            $table->string('medication')->nullable();
+            $table->string('purpose_of_medication')->nullable();
+            $table->text('use_schedule')->nullable(); // Can be JSON or TEXT based on your requirement
+            $table->string('food_use')->nullable();
+            $table->string('dose_use')->nullable();
+            $table->string('doses_per_package')->nullable();
+            $table->date('last_prescription_start')->nullable();
             $table->timestamps();
 
             $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
